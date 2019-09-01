c059f61f972054368eb1ebc189843667b05d47f2
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2019-09-01 01:32:17 -0400

    fixed workspaces in bar crashing by blocking movement to certain spaces

--- scripts/changeSpaces.sh
+++ scripts/changeSpaces.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 index=$1; 
-display=$(yabai -m query --displays | jq --argjson index "${index}" -r 'map(select(.spaces[] | contains($index))) | .[] .index');
-eval "$(yabai -m display --focus ${display} && yabai -m query --spaces | jq --argjson index "${index}" -r '(.[] | select(.index == $index).windows[0]) as $wid | if $wid then "yabai -m window --focus \"" + ($wid | tostring) + "\"" else "skhd --key \"ctrl - " + (map(select(."native-fullscreen" == 0)) | index(map(select(.index == $index))) + 1 % 10 | tostring) + "\"" end' && osascript -e 'tell application "Übersicht" to refresh widget id "workspaces-jsx"')"
-
-
+banned=$(yabai -m query --spaces | jq -jr '.[] | select((.visible == 1) and (.windows | length == 0)).index')
+if [[ $index != $banned ]]; then
+    display=$(yabai -m query --displays | jq --argjson index "${index}" -r 'map(select(.spaces[] | contains($index))) | .[] .index');
+    eval "$(yabai -m display --focus ${display} && yabai -m query --spaces | jq --argjson index "${index}" -r '(.[] | select(.index == $index).windows[0]) as $wid | if $wid then "yabai -m window --focus \"" + ($wid | tostring) + "\"" else "skhd --key \"ctrl - " + (map(select(."native-fullscreen" == 0)) | index(map(select(.index == $index))) + 1 % 10 | tostring) + "\"" end' && osascript -e 'tell application "Übersicht" to refresh widget id "workspaces-jsx"')"
+fi
