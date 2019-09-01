86ba1993f3f2de8da3255645b30f084566e60580
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2019-09-01 00:33:56 -0400

    calculated where displays split spaces

--- widgets/scripts/spaces.py
+++ widgets/scripts/spaces.py
@@ -5,8 +5,8 @@ import re
 import json
 
 
-def get_spaces():
-    ps = subprocess.Popen(['yabai', '-m', 'query', '--spaces'], stdout=subprocess.PIPE).communicate()[0].replace('\n', '').replace('\t', '').replace('[{', '').replace('}]', '').split('},{')
+def get_yabai(module):
+    ps = subprocess.Popen(['yabai', '-m', 'query', '--%s' % module], stdout=subprocess.PIPE).communicate()[0].replace('\n', '').replace('\t', '').replace('[{', '').replace('}]', '').split('},{')
     result = {}
     for k, item in enumerate(ps):
         item = "{" + item + "}".replace("'", "\"")
@@ -14,16 +14,8 @@ def get_spaces():
         result[k] = json_string
     return result
 
-def get_windows():
-    ps = subprocess.Popen(['yabai', '-m', 'query', '--windows'], stdout=subprocess.PIPE).communicate()[0].replace('\n', '').replace('\t', '').replace('[{', '').replace('}]', '').split('},{')
-    result = {}
-    for k, item in enumerate(ps):
-        item = "{" + item + "}".replace("'", "\"")
-        json_string = json.loads(item)
-        result[k] = json_string
-    return result
 
-def prettify_results(spaces, windows):
+def prettify_results(spaces, windows, displays):
     active_window = json.loads(subprocess.Popen(['yabai', '-m',  'query', '--windows', '--window'], stdout=subprocess.PIPE).communicate()[0].replace('\n', '').replace('\t', '').replace("'", "\""))
     result = {}
     for space in spaces:
@@ -39,15 +31,25 @@ def prettify_results(spaces, windows):
         for innerkey, innerval in outerval.items():
             if innerval not in filtered[outerkey].values():
                 filtered[outerkey][innerkey] = innerval
+
     active_space = filtered[str(active_window['space']-1)]
     for id, app in active_space.items():
         if app == active_window['app']:
             filtered['active'] = {id: app}
+
+    if len(displays.keys()) > 1:
+        display_splits = []
+        for disp_id in displays.keys()[1:]:
+            display_splits.append(displays[disp_id]['spaces'][0])
+        filtered['display_splits'] = display_splits
+
     return filtered
 
 
 def main():
-    return json.dumps(prettify_results(get_spaces(), get_windows()))
+    modules = ['spaces', 'windows', 'displays']
+    outputs = list(map(get_yabai, modules))
+    return json.dumps(prettify_results(*outputs))
 
 
 if __name__ == '__main__':
